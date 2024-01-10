2c656767bac67e65ef62c13aaa4a969870a6eaec
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2024-01-10 17:15:57 -0600

    ui refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,10 @@
-import React, { Fragment, useEffect } from "react";
-import axios from "axios";
+import React, { Fragment } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Main from "./components/Main";
 
 function App() {
-  useEffect(() => {
-    fetchToken();
-
-    const tokenRefreshInterval = setInterval(() => {
-      fetchToken();
-    }, 5000000 * 1000);
-
-    return () => clearInterval(tokenRefreshInterval);
-  }, []);
-
-  const fetchToken = async () => {
-    try {
-      await axios.get(`/api/refresh-token`);
-    } catch (error) {
-      console.error("Error refreshing token ", error);
-    }
-  };
-
+ 
   return (
     <Fragment>
       <Main />
